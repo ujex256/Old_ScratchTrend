90c62c6d69d29fce6ff4c33ab4a1fe701e2ecd2e
henji <105550500+henji243@users.noreply.github.com>
2022-09-02 22:15:53 +0900

    ヘッドレスモードなど
    ・ヘッドレスモード(ウィンドウを表示させない)にしました。
    ・デバッグで使った部分が完全に消されていませんでした。
    ・第2引数を指定するときの"," の後に1つスペースを入れました。

--- trend.py
+++ trend.py
@@ -6,13 +6,15 @@ import chromedriver_binary,time
 
 def startup():
     option = webdriver.ChromeOptions()
-    option.add_experimental_option('excludeSwitches', ['enable-logging'])  #鬱陶しいseleniumのログを非表示にする
+    option.add_experimental_option('excludeSwitches', ['enable-logging'])  # 鬱陶しいログを非表示にする
+    option.add_argument('--headless')  # ヘッドレスモード(ウィンドウを非表示)
     driver = webdriver.Chrome(options=option)
     driver.get("https://scratch.mit.edu/explore/projects/all")
     time.sleep(1.5)
     return driver
 
-def get_trends_by_num(start,end):
+
+def get_trends_by_num(start, end):
     """Scratchの傾向の作品のタイトルとIDを辞書型で返します。
     start引数とend引数に順位を指定してください。"""
     if start > end:
@@ -21,27 +23,26 @@ def get_trends_by_num(start,end):
     driver = startup()
     # 指定された順位が1Pより下ならそこまで表示させる
     if end - start >= 16:
-        print(ceil((end-start) / 16))
         for j in range(ceil((end-start)/16)):
             driver.find_element(By.XPATH, '//*[@id="projectBox"]/button/span').click()
-            time.sleep(1.25)
+            time.sleep(1)
 
-    soup = BeautifulSoup(driver.page_source,'html.parser')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     trend = list()
 
-    for i in range(start,end):
+    for i in range(start, end):
         if i % 16 == 0:
             driver.find_element(By.XPATH, '//*[@id="projectBox"]/button/span').click()
-            time.sleep(2)
-            soup = BeautifulSoup(driver.page_source,'html.parser')
+            time.sleep(1)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
         # CSSセレクタで選択し、タイトルとIDを抽出
         found = soup.select_one(f"#projectBox > div > div > div:nth-of-type({i}) > div > div > a")
-        project_data = {'title':found.contents[0], 'id':found.attrs['href'].replace("/","").replace("projects","")}
+        project_data = {'title': found.contents[0], 'id': found.attrs['href'].replace("/", "").replace("projects", "")}
         trend.append(project_data)
     return trend
 
 
-def get_trends_by_page(start,end):
+def get_trends_by_page(start, end):
     """Scratchの傾向の作品のタイトルとIDを辞書型で返します。
     ページをstart引数とend引数に指定してください。"""
     if start > end:
@@ -52,26 +53,26 @@ def get_trends_by_page(start,end):
     if start >= 2:
         for i in range(start-1):
             driver.find_element(By.XPATH, '//*[@id="projectBox"]/button/span').click()
-            time.sleep(1.25)
+            time.sleep(1)
 
-    soup = BeautifulSoup(driver.page_source,'html.parser')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     trend = list()
 
-    for i in range((start-1)*16 + 1, end*16+1):
+    for i in range((start-1)*16 + 1, end*16 + 1):
         if i % 16 == 0:
             driver.find_element(By.XPATH, '//*[@id="projectBox"]/button/span').click()
-            time.sleep(2)
-            soup = BeautifulSoup(driver.page_source,'html.parser')
+            time.sleep(1)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
         # CSSセレクタで選択し、タイトルとIDを抽出
         # get_trends_by_num関数と同じ処理をしているので関数にしたかったがややこしくなるなのでしていない
         found = soup.select_one(f"#projectBox > div > div > div:nth-of-type({i}) > div > div > a")
-        project_data = {'title':found.contents[0], 'id':found.attrs['href'].replace("/","").replace("projects","")}
+        project_data = {'title': found.contents[0], 'id': found.attrs['href'].replace("/", "").replace("projects", "")}
         trend.append(project_data)
     return trend
 
 
 if __name__ == "__main__":
     # Test
-    res = get_trends_by_page(start=1,end=3)
+    res = get_trends_by_page(start=1, end=3)
     print(*res, sep='\n')
     print(len(res))
